add6aac68424d2bade337db3013ce032fbe6e017
Pavel <pavel.fedorov@n1affiliates.com>
2023-08-14 11:40:32 +0300

    start project

--- libs-links.js
+++ libs-links.js
@@ -1,5 +1,4 @@
 export const libsJsLink = [
-    //'node_modules/jquery/dist/jquery.min.js',
     'node_modules/svg4everybody/dist/svg4everybody.min.js',
     'node_modules/swiper/swiper-bundle.min.js',
     //'node_modules/pristinejs/dist/pristine.min.js',
