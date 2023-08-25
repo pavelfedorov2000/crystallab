7939fc46fadb80116a27f530875167d7ba96de65
Pavel Fedorov <p.fedorov@imedia.by>
2023-08-25 20:45:00 +0300

    25.08 wip

--- app/blocks/tab/tab.js
+++ app/blocks/tab/tab.js
@@ -21,21 +21,5 @@ app.tabs = {
             tabsContentActive.classList.remove('active');
             tabsContent.classList.add('active');
         });
-
-        /* $('.tab').on('click', function () {
-            const $tab = $(this);
-            const $tabNotActive = $tab.closest('.tabs').find('.tab.active');
-            const $tabsContent = $(`#${$tab.attr('aria-controls')}`);
-            const $tabsContentNotActive = $(`#${$tabNotActive.attr('aria-controls')}`);
-
-            $tabNotActive.removeClass('active');
-            $tabNotActive.attr('aria-selected', false);
-
-            $tab.addClass('active');
-            $tab.attr('aria-selected', true);
-
-            $tabsContentNotActive.hide();
-            $tabsContent.fadeIn();
-        }); */
     },
 };
